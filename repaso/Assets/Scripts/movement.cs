a639754716c27d3e1d7ef592fab0a032dc88c79d
Julio020 <julio.metro07@gmail.com>
2023-09-27 10:19:25 +0200

    Actualizacion de trabajo inicial script movimineto

--- repaso/Assets/Scripts/movement.cs
+++ repaso/Assets/Scripts/movement.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class movement : MonoBehaviour
 {
+
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb= GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
     {
         
     }
